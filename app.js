fa0666f724af3c47a478d46fe36a123004e1ad33
AHTEShAM <ahteshamarain3@gmail.com>
2023-07-22 23:41:04 +0500

    add value with dom

---
+++ app.js
@@ -0,0 +1,7 @@
+function add(){
+    let fn=document.getElementById("fn").value
+    let sn=document.getElementById("sn").value
+    let ans=fn+sn
+    document.getElementById("total").innerHTML=ans
+
+}
